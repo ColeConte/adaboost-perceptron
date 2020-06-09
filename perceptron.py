4d7509fe999bb3a0daf8e1a64821555e0f9466dc
ColeConte <colefconte@gmail.com>
2020-06-08 17:12:18 -0700

    perceptron algorithm implemented

--- perceptron.py
+++ perceptron.py
@@ -4,11 +4,32 @@ perceptron.py
 '''
 
 import argparse
+import pandas as pd
+import numpy as np
+from itertools import count
+
+def perceptron(df):
+	w = pd.DataFrame(np.zeros((1,len(df.columns)-1)))
+	x = df.iloc[:,:-1]
+	y = df.iloc[:,-1]
+	y = y.replace(0,-1)
+	w.columns = x.columns.values
+	for t in count():
+		for i in range(len(df)):
+			if (x.iloc[i].dot(w.iloc[0]))*y.iloc[i] <= 0:
+				w = pd.DataFrame(w.iloc[0] + (y.iloc[i]*x.iloc[i]))
+				w = w.T
+				print(i)
+				break
+		if i == (len(df)-1):
+			return w
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset")
 parser.add_argument("--mode")
 args = parser.parse_args()
-print(args.dataset)
+
+df = pd.read_csv(args.dataset)
+perceptron(df)
 
 
--- readme.txt
+++ readme.txt
@@ -1,8 +1,15 @@
 Cole Conte CSE 512 HW 1
 readme.txt
 
+4. README Section
 perceptron.py usage:
 python perceptron.py --dataset /path/to/data/filename.csv --mode erm
 mode can be erm or cv
 
 adaboost.py follows the same pattern
+
+
+
+1.
+2. I observed that the perceptron algorithm did not terminate even after several minutes. This makes sense for a non-linearly separable data set; the algorithm will continually update its weights until all the points are separated, but because not all points are linearly separable, the algorithm will just cycle.
+3.
